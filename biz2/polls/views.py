a53ca5f728ab0ec91caa8d329cd3503b3ab67593
rushabhmshah99 <rushabhmshah99@gmail.com>
2016-01-08 22:52:36 +0530

    A

---
+++ biz2/polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+# Create your views here.
+def addition(request):
+	if request.POST:
+		num1 = request.POST['num1']
+		num2 = request.POST['num2']
+		ress = eval(num1) + eval(num2)
+		return render_to_response('result.html', {'result': ress}, context_instance=RequestContext(request))
+	else:
+		return render_to_response('result.html', {}, context_instance=RequestContext(request))
